49673163d77c55040afec01aae0a004e2691640e
Matthew Hale <Matth3w4000@gmail.com>
2021-01-14 14:42:52 +0000

    Exercises done

--- src/Lab.hs
+++ src/Lab.hs
@@ -7,7 +7,7 @@ module Lab where
 
 --------------------------------------------------------------------------------
 
-import Data.Char (isDigit)
+import Data.Char (isDigit, toLower)
 
 --------------------------------------------------------------------------------
 
@@ -18,19 +18,21 @@ numbers = 4 : 8 : 15 : 16 : 23 : 42 : []
 -- with explicit brackets to show the associativity of cons
 -- the number 4 is currently missing from the beginning of this list
 numbers' :: [Int]
-numbers' = 8 : (15 : (16 : 23 : (42 : [])))
+numbers' = 4 : (8 : (15 : (16 : 23 : (42 : []))))
 
 -- syntactic sugar for lists
 -- the number 42 is currently missing from the end of this list
 numbers'' :: [Int]
-numbers'' = [4,8,15,16,23]
+numbers'' = [4,8,15,16,23,42]
 
 --------------------------------------------------------------------------------
 
 isPalindrome :: [Char] -> Bool 
-isPalindrome = undefined
+isPalindrome x | x == reverse x = True
+    | otherwise = False
 
+--neatest code you'll ever see
 validModuleCode :: [Char] -> Bool 
-validModuleCode = undefined
+validModuleCode x = ([toLower (head x) , toLower(head (tail x))] == "cs") && (isDigit (head (tail (tail x))) && isDigit (head (tail (tail (tail x)))) && isDigit (head (tail (tail (tail (tail x))))))
 
 --------------------------------------------------------------------------------
